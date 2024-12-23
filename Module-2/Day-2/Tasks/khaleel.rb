b7b656fcb8fc74ebc3d7420f483be72723f605bb
khaleel <Khaleeelshaik0405@gmail.com>
2024-12-23 13:59:26 +0530

    [M2D2] Add tasks for day 2

---
+++ Module-2/Day-2/Tasks/khaleel.rb
@@ -0,0 +1,22 @@
+# Local Variable 
+  local_varaible = "This is my first local variable"
+  _local = "This is my another local variable"
+# Global Variable
+  $my_global_variable = "Global variable"
+
+  #class to demonstrate instance and class variable
+  class Tata
+    def intialise()
+      # Class Variable
+        @@class_variable = "My class variable"
+      # Instance Variable
+        @instace_variable = "My Instance variable"
+    end
+  end
+
+# multi line strings
+
+  multiline_string = "This is first line
+This is second line"
+  
+  puts multiline_string
