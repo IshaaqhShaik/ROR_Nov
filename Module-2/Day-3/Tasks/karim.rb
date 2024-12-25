ab08f6a8d034405b43ed6edad8e55596efdc86fe
Dafiya <sikarimulla@gmail.com>
2024-12-25 18:57:42 +0530

    Day-3 Task and Notes of Module-2

---
+++ Module-2/Day-3/Tasks/karim.rb
@@ -0,0 +1,53 @@
+#Arithmetic Operators
+puts "Addition: (2 + 3)"
+puts (2 + 3)
+
+puts "Subtraction: (5 - 3)"
+puts (5 - 3)
+
+puts "Multiplication: (4 * 6)"
+puts (4 * 6)
+
+puts "Divison: (66 / 4)"
+puts (66 / 4)
+
+puts "Modulo Divison: (8 % 3)"
+puts (8 % 3)
+
+#Comparison Operators
+puts "Equal to(==): 67==67 and 56==65"
+puts 67==67
+puts 56==65
+
+puts "Not Equal to(!=): 67!=67 and 56!=65"
+puts 67!=67
+puts 56!=65
+
+puts "Greater than(>): 67>67 and 65>56"
+puts 67>67
+puts 65>56
+
+puts "Less than(<): 67<67 and 56<65"
+puts 67<67
+puts 56<65
+
+puts "Greater than or Equal to(>=): 67>=67 and 56>=65"
+puts 67>=67
+puts 56>=65
+
+puts "Less than or Equal to(<=): 67<=67 and 65<=56"
+puts 67<=67
+puts 65<=56
+
+#Logical Operators
+puts "And (&&): 6>3 && 7<9 and 5>9 && 6<3"
+puts 6>3 && 7<9
+puts 5>9 && 6<3
+
+puts "Or (||): 6>3 || 7<9 and 5>9 || 6<3"
+puts 6>3 || 7<9
+puts 5>9 || 6<3
+
+puts "Not (!): !(6>3) and !(5<3)"
+puts !(6>3)
+puts !(5<3)
