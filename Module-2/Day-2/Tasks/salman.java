8d6eb1250302598366175961cd665ac179c13382
salman <syedsalman4341@gmail.com>
2024-12-30 21:00:47 +0530

    add M2D2 notes,java,ruby and cheatsheet

---
+++ Module-2/Day-2/Tasks/salman.java
@@ -0,0 +1,31 @@
+// Java Program to show the use of
+// Instance Variables
+import java.io.*;
+
+class GFG {
+
+    // Declared Instance Variable
+    public String geek;
+    public int i;
+    public Integer I;
+    public GFG()
+    {
+        // Default Constructor
+        // initializing Instance Variable
+        this.geek = "Syed salman";
+    }
+
+    // Main Method
+    public static void main(String[] args)
+    {
+        // Object Creation
+        GFG name = new GFG();
+
+        // Displaying O/P
+        System.out.println("Geek name is: " + name.geek);
+        System.out.println("Default value for int is "+ name.i);
+      
+        // toString() called internally
+        System.out.println("Default value for Integer is "+ name.I);
+    }
+}
---
+++ Module-2/Day-2/Tasks/salmanvar.rb
@@ -0,0 +1,31 @@
+class Mult
+  @@no_of_methods=0
+  def initialize()
+      @first_number=5
+      @second_number=6
+
+      @@no_of_methods=@@no_of_methods+1
+      puts "No of methods is : #@@no_of_methods"
+  end
+  
+  def print_first_number
+      puts "The First Number is : #@first_number"
+  end
+
+  def print_second_number        
+      puts "The Second Number is : #@second_number"
+  end
+
+  def print_multiplication              
+      puts "Multiplication of First Number and Second Number is :"
+      return @first_number *  @second_number 
+  end
+
+end
+first_method=Mult.new()
+#puts print.class
+second_method=Mult.new()
+multiplication=Mult.new()
+puts first_method.print_first_number
+puts second_method.print_second_number
+puts multiplication.print_multiplication
